98c7a4ce252c2861c7c52d0168397a5f00594ef3
ivanrubic <147489549+ivanrubic@users.noreply.github.com>
2025-03-17 19:10:00 +0100

    Update NewClass.cs

--- NewClass.cs
+++ NewClass.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 using System.Threading.Tasks;
 
 namespace ConsoleApp2
@@ -10,3 +11,4 @@ namespace ConsoleApp2
     {
     }
 }
+//Nova Linija
